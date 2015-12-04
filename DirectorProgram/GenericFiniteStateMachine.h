dc6136be574393c0aba67d14b05efa44d3ac4e1f
haoyuhan27 <haoyuhan27@bitbucket.org>
2015-12-04 00:38:05 -0600

    director comment updated

--- DirectorProgram/GenericFiniteStateMachine.h
+++ DirectorProgram/GenericFiniteStateMachine.h
@@ -1,4 +1,12 @@
-﻿#pragma once
+﻿//
+//Model a generic finite state machine to model the state transition model in this project.
+//The purpose of designing such mechanism is to make sure every state is seperated and the job 
+//of each state is clearly defined.
+//
+
+#ifndef GENERIC_FINITE_STATE_MACHINE_H
+#define GENERIC_FINITE_STATE_MACHINE_H
+
 #include <vector>
 #include <unordered_map>
 #include <functional>
@@ -6,11 +14,20 @@
 template<typename StateCode, typename InputCode>
 class GenericFiniteStateMachine {
 public:
+
+	//virtual deconstructor
     virtual ~GenericFiniteStateMachine() {
     }
 
+	//
+	//enum class RetCode represents if the current state
+	//can go to the next state or not or it is staying.
+	//
     enum RetCode { kOk, kFail, kRepeat };
 
+	//
+	//Simulate the transition between two state
+	//
     struct Transition {
         StateCode src_state;
         RetCode   ret_code;
@@ -20,18 +37,22 @@ public:
         {}
     };
 
+	//Constructor
     GenericFiniteStateMachine(const StateCode& cur_state) : 
         cur_state(cur_state), rc() 
     {}
 
+	//Add a transtion rule to the transition vector
     void add_transition(StateCode src, RetCode rc, StateCode dst) {
         transitions.push_back(Transition(src, rc, dst));
     }
 
+	//add a state function
     void add_state_function(StateCode code, std::function<RetCode(InputCode)> func) {
         state_map[code] = func;
     }
 
+	//look up the result given the current state and the transition
     StateCode lookup_transition(StateCode src, RetCode rc) {
         for (size_t i = 0; i < transitions.size(); ++i) {
             if (src == transitions[i].src_state && rc == transitions[i].ret_code)
@@ -40,6 +61,7 @@ public:
         return StateCode();
     }
 
+	//run the state machine given the input state
     void run(InputCode input) {
         state_fun = state_map[cur_state];
         rc = state_fun(input);
@@ -47,9 +69,11 @@ public:
     }
 
 protected:
+
     // Override this to add finite state machine rules here
     virtual void on_machine_setup() = 0;
 
+	//protected setup method
     void setup() {
         on_machine_setup();
     }
@@ -63,3 +87,6 @@ private:
     RetCode rc;
     std::function<RetCode(InputCode)> state_fun;
 };
+
+
+#endif
--- DirectorProgram/SignalEventHandler.h
+++ DirectorProgram/SignalEventHandler.h
@@ -1,3 +1,9 @@
+//
+//Signal Event Handler
+//This class is inherited from ACE_EVENT_HANDLER to handle the signal interrupt Ctrl-C
+//
+
+
 #ifndef SIGNAL_EVENT_HANDLER_H
 #define SIGNAL_EVENT_HANDLER_H
 #include <ace/Event_Handler.h>
@@ -8,15 +14,20 @@
 class SignalEventHandler : public ACE_Event_Handler {
 public:
     
-
+	//default constructor
+	//have some debug info printed
     SignalEventHandler() {
         DEBUG_PRINTF("[%p]SignalEventHandler Allocated\n", this);
     }
 
+	//default deconstructor
+	//have some debug info printed
     ~SignalEventHandler() {
         DEBUG_PRINTF("[%p]SignalEventHandler Deallocated\n", this);
     }
 
+	//override handle_signal deal with ctrl c 
+	//and when user hit ctrl c, it should end the event loop
     virtual int handle_signal(int signum, siginfo_t*, ucontext_t*) override {
         if (signum != SIGINT) return 0;
 
