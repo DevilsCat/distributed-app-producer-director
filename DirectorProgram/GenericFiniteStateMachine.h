2f22b68ffd1f3fc02305d0488856775e97f2448d
DevilsCat <xiaoyuxqx@gmail.com>
2016-06-24 23:40:35 -0700

    fix indents

--- DirectorProgram/GenericFiniteStateMachine.h
+++ DirectorProgram/GenericFiniteStateMachine.h
@@ -15,19 +15,19 @@ template<typename StateCode, typename InputCode>
 class GenericFiniteStateMachine {
 public:
 
-	//virtual deconstructor
+    //virtual deconstructor
     virtual ~GenericFiniteStateMachine() {
     }
 
-	//
-	//enum class RetCode represents if the current state
-	//can go to the next state or not or it is staying.
-	//
+    //
+    //enum class RetCode represents if the current state
+    //can go to the next state or not or it is staying.
+    //
     enum RetCode { kOk, kFail, kRepeat };
 
-	//
-	//Simulate the transition between two state
-	//
+    //
+    //Simulate the transition between two state
+    //
     struct Transition {
         StateCode src_state;
         RetCode   ret_code;
@@ -37,22 +37,22 @@ public:
         {}
     };
 
-	//Constructor
+    //Constructor
     GenericFiniteStateMachine(const StateCode& cur_state) : 
         cur_state(cur_state), rc() 
     {}
 
-	//Add a transtion rule to the transition vector
+    //Add a transtion rule to the transition vector
     void add_transition(StateCode src, RetCode rc, StateCode dst) {
         transitions.push_back(Transition(src, rc, dst));
     }
 
-	//add a state function
+    //add a state function
     void add_state_function(StateCode code, std::function<RetCode(InputCode)> func) {
         state_map[code] = func;
     }
 
-	//look up the result given the current state and the transition
+    //look up the result given the current state and the transition
     StateCode lookup_transition(StateCode src, RetCode rc) {
         for (size_t i = 0; i < transitions.size(); ++i) {
             if (src == transitions[i].src_state && rc == transitions[i].ret_code)
@@ -61,7 +61,7 @@ public:
         return StateCode();
     }
 
-	//run the state machine given the input state
+    //run the state machine given the input state
     void run(InputCode input) {
         state_fun = state_map[cur_state];
         rc = state_fun(input);
@@ -73,7 +73,7 @@ protected:
     // Override this to add finite state machine rules here
     virtual void on_machine_setup() = 0;
 
-	//protected setup method
+    //protected setup method
     void setup() {
         on_machine_setup();
     }
